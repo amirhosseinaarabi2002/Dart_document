cdfa242e30c3d7dc2ce55032abcb758cc2141bae
unknown <amiraarabisw2002@gmail.com>
2025-05-31 12:16:26 +0330

    finish sets and start sets

--- project-test/bin/project_test.dart
+++ project-test/bin/project_test.dart
@@ -202,17 +202,104 @@ void main(List<String> arguments) {
   print(lst15);
 
   // name constructor unmodifible
-  List lst13 = [1,2,3];
+  List lst13 = [1, 2, 3];
   List lst14 = List.unmodifiable(lst13); // it means w can not change it
   // lst14.add(25);
   print(lst14);
 
   // list null safety
-  List <String>? lst16;
-  List <String?> lst17 = ["amir", null];
+  List<String>? lst16;
+  List<String?> lst17 = ["amir", null];
 
   print(lst16);
 
+  // methodes that we use in lists
+  List lst18 = ["amirhossein"];
+  lst18.add("0190");
+  lst18.addAll([1, 2, 3]); // iterable means we should insert a list as value
 
+  lst18.insert(1, "ali"); // we give index and element
+  lst18.insertAll(3, [00, 11, 22]); // its itarable
+
+  bool res1 = lst18.remove(00); // remove value in the list
+  print(res1);
+
+  var res2 = lst18.removeAt(2); // remove index in the list
+  print(res2);
+
+  var res3 = lst18.removeLast(); // remove last item in the list
+  print(res3);
+
+  List<int> lst19 = [1, 2, 3, 4, 5, 6];
+  lst19.removeRange(2, 4); // remove range from to from item in the list
+
+  lst19.sublist(1);
+
+  print(lst19);
+
+  print(lst18);
+
+  List<int> lst21 = [1, 2, 3, 4, 5, 6];
+  lst21.shuffle(); // it makes our list unorganized
+
+  print(lst21);
+
+  List <String> lst22 = ["ali", "amir", "pouya"];
+  Map <int, String> map1 = lst22.asMap(); // it make keys for our value in the list
+
+  print(map1);
+
+  List mixList = ["a", 1, "b", 2];
+  var mixi = mixList.whereType<String>(); // it shows the values with types that we want 
+
+  print(mixi);
+
+  List<int> lst23 = [1, 2, 3, 4, 5, 6];
+  lst23.replaceRange(3, 4, [30, 40]);
+
+  print(lst23);
+
+  List<int> lst24 = [1, 2, 3, 4, 5, 6];
+  print(lst24.firstWhere((i) => i > 3));
+  print(lst24.lastWhere((i) => i > 3));
+  print(lst24.singleWhere((i) => i == 3)); // if we had more than one 3 in the list it returned error
+
+  var x = lst24.fold(10, (i, j) => i + j); // 10 + 1 + 2 + 3 + ... = 31
+  var xy = lst24.reduce((i, j) => i + j); // 1 + 2 + 3 + ... = 31
+  print(x);
+  print(xy);
+
+  List <String> sportList = ["football", "soccer"];
+  print(sportList.followedBy(["tennis", "swim"]).toList()); // add new items to end of the list
+
+  print(sportList.any((i) => i.contains("soccer"))); // true
   
+  print(sportList.every((i) => i.startsWith("c"))); // false
+
+  print(sportList.take(1)); //take one item from my list and show it
+
+  print(sportList.fold<String>("golf", (i, j) => i + j)); // concat all of the string items in the list
+
+
+  // list properties
+  List lst20 = [];
+  lst20.add(1);
+  lst20.add(2);
+
+  print(lst20.first); // index[0] ind list 
+  print(lst20.isEmpty);
+  print(lst20.isNotEmpty);
+  print(lst20.length);
+  print(lst20.last);
+  print(lst20.reversed);
+  // print(lst20.single); //  if it had only 1 element otherwise it returns error
+
+
+  // Sets
+  // 2 diffrences with list : 1. we use {} for sets 2. in set , set removes repeated items and count them only one time
+
+  Set<String> names = Set.from(["ali", "mmd", "hesam"]);
+  names.add("reza");
+
+  print(names);
 }
